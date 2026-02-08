ee13d2566481af047a266275df16e3a0a44a9f1d
makpal80 <makpalamangalieva80@gmail.com>
2026-02-08 21:06:58 +0500

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -8,6 +8,8 @@ ALGORITHM = "HS256"
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
+    print("DEBUG password repr:", repr(password))
+    print("DEBUG password len bytes:", len(password.encode("utf-8")))
     return pwd_context.hash(password)
 
 def verify_password(password: str, hashed_password: str):
