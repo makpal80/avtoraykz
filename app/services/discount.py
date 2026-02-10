06ffff626053ab4924934cc613cadb7e639cd15d
makpal80 <makpalamangalieva80@gmail.com>
2026-02-10 22:41:32 +0500

    Update discount.py v2

--- app/services/discount.py
+++ app/services/discount.py
@@ -1,11 +1,11 @@
 def calculate_discount(orders_count: int) -> int:
     if orders_count <= 1:
         return 3
-    elif orders_count == 2:
+    if orders_count == 2:
         return 4
-    elif orders_count == 3:
+    if orders_count == 3:
         return 5
-    elif orders_count == 4:
+    if orders_count == 4:
         return 7
     else:
         return 10
